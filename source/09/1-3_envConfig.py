6a081601e002858249c3e1da18f3add229f7b19c
SeohuiJeong0420 <ims8289@naver.com>
2025-07-01 18:00:21 +0900

    generativeAI_RPA

---
+++ source/09/1-3_envConfig.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from decouple import config
+import os
+# 방법1
+load_dotenv(".env")
+client_id = os.getenv("CLIENT_ID")
+print('방법1 :', client_id)
+# 방법2
+client_id = config('CLIENT_ID')
+print('방법2 :', client_id)
