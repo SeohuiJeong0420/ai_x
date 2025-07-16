eaedb7e35b4abd5a23090a2fd787b0e79669f560
SeohuiJeong0420 <ims8289@naver.com>
2025-07-16 11:55:03 +0900

    flask6

--- source/11_flask/ch3_methods/app.py
+++ source/11_flask/ch3_methods/app.py
@@ -34,3 +34,8 @@ def join():
 @app.route("/update/<name>/<id>/<pw>/<addr>", methods=["PATCH"])
 def update(name, id, pw, addr):
     return f"{name}님 정보가 수정되었습니다"
+
+@app.route("/delete/<id>", methods=["DELETE"])
+def delete(id):
+    # delete from 테이블명 where id = id를 DBMS에 전송하기
+    return f"{id}번째 사용자를 삭제하였습니다"
