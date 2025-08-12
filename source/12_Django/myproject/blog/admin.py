18cd210e923b743c0053a601e430ff07ed666d81
SeohuiJeong0420 <ims8289@naver.com>
2025-08-12 11:30:49 +0900

    Django6

--- source/12_Django/myproject/blog/admin.py
+++ source/12_Django/myproject/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
-
+from blog.models import Post, Comment
 # Register your models here.
-admin.site.register(Post) #admin페이지에서 Post테이블 엑세스 가능
+admin.site.register(Post) # admin 페이지에서 Post 테이블 액세스 가능
+admin.site.register(Comment) # admin페이지에서 Commentss
